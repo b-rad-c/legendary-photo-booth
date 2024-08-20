b78117698ad0cc431d40726519a7f091452e2d48
Brad Corlett <brad.corlett@gmail.com>
2024-08-19 21:27:51 -0700

    checkpoint

--- photo_booth.py
+++ photo_booth.py
@@ -21,7 +21,14 @@ from PyQt5.QtWidgets import (
 from picamera2 import Picamera2
 from picamera2.previews.qt import QGlPicamera2
 
-# change this to change where photos are saved
+#
+# config
+#
+
+pic_width = 1200
+pic_height = 900
+window_width = 1800
+window_height = 900
 photo_dump = Path('/home/olmec/Desktop')
 
 #
@@ -29,37 +36,35 @@ photo_dump = Path('/home/olmec/Desktop')
 #
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"size": (1200, 900)}))
+still_cfg = picam2.create_still_configuration()
+preview_cfg = picam2.create_preview_configuration(main={'size': (pic_width, pic_height)})
+picam2.configure(preview_cfg)
+
 
 #
 # init gui app
 #
 
 app = QApplication([])
-qpicamera2 = QGlPicamera2(picam2, width=1200, height=900, keep_ar=False)
+qpicamera2 = QGlPicamera2(picam2, width=pic_width, height=pic_height, keep_ar=False)
 window = QWidget()
 
 #
 # input handlers
 #
 
-n = 0
 def start_picture_capture():
-    global n
     button.setEnabled(False)
-    cfg = picam2.create_still_configuration()
 
     # change this to change how the folders and names are created for saved pics
     photo_path = photo_dump / datetime.datetime.now().strftime('%Y-%b-%d-%a/%H-%M-%f.jpg')
     os.makedirs(photo_path.parent, exist_ok=True)
 
-    picam2.switch_mode_and_capture_file(cfg, photo_path, signal_function=qpicamera2.signal_done, delay=30)
+    picam2.switch_mode_and_capture_file(still_cfg, photo_path, signal_function=qpicamera2.signal_done, delay=30)
 
 def finish_picture_capture(job):
-    global n
     picam2.wait(job)
     button.setEnabled(True)
-    n += 1
     
 def keyboard_handler(e):
     if e.key() == Qt.Key_Q:
@@ -75,7 +80,7 @@ def keyboard_handler(e):
 qpicamera2.done_signal.connect(finish_picture_capture)
 window.keyPressEvent = keyboard_handler
 
-button = QPushButton("Click to capture JPEG")
+button = QPushButton('Click to capture JPEG')
 button.clicked.connect(start_picture_capture)
 
 layout_v = QVBoxLayout()
@@ -85,8 +90,8 @@ layout_h = QHBoxLayout()
 layout_h.addWidget(qpicamera2, 80)
 layout_h.addLayout(layout_v, 20)
 
-window.setWindowTitle("Legendary Selfie")
-window.resize(1800, 900)
+window.setWindowTitle('Legendary Selfie')
+window.resize(window_width, window_height)
 window.setLayout(layout_h)
 
 picam2.start()
